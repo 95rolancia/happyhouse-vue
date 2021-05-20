2b669326c7431d276989371252f0c0a6f656b07c
jangjunhyeok <95jjangjun@gmail.com>
2021-05-20 23:44:46 +0900

    Create Header & Add Header in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
   <div id="app">
+    <Header />
     <router-view />
   </div>
 </template>
+<script>
+import Header from "@/components/Header.vue";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
 
 <style>
 #app {
@@ -11,18 +20,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="header">
+    <section class="header-right"></section>
+  </header>
+</template>
+<script>
+export default {};
+</script>
+<style></style>
