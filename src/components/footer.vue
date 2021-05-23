e5113b830927bb98d781de872f7c536749c5e4c7
jangjunhyeok <95jjangjun@gmail.com>
2021-05-23 14:23:38 +0900

    Create Footer & Add Footer in MainPage

---
+++ src/components/footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <footer class="footer">
+    <ul class="footer-menu">
+      <li class="footer-menu-item">이용약관</li>
+      <li class="footer-menu-item">매물신고운영</li>
+      <li class="footer-menu-item">개인정보처리방침</li>
+      <li class="footer-menu-item">부동산 고객센터</li>
+    </ul>
+    <p class="footer-content">
+      Happy house에서 제공하는 부동산 정보는 각 콘텐츠 제공업체로부터 받는 정보로 참고용입니다.<br />
+      정보의 정보의 정확성이나 신뢰성을 보증하지 않으며, 서비스 이용의 결과에 대해서 어떠한 법적인
+      책임을 지지 않습니다. <br />게시된 정보는 무단으로 배포할 수 없습니다.
+    </p>
+    <p class="footer-content">Copyright ⓒ Happy House. All Rights Reserved.</p>
+  </footer>
+</template>
+<script>
+export default {};
+</script>
+<style>
+.footer {
+  position: absolute;
+  bottom: -5.7em;
+  width: 100%;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0.5em 0em;
+  color: #ffebee;
+  background-color: darkslateblue;
+}
+
+.footer-menu {
+  display: flex;
+}
+
+.footer-menu-item {
+  margin: 1em;
+  cursor: pointer;
+}
+
+.footer-menu-item:hover {
+  color: orange;
+}
+
+.footer-content {
+  font-size: 0.8rem;
+}
+</style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,16 +1,30 @@
 <template>
-  <section class="main"></section>
+  <section class="main">
+    <Footer />
+  </section>
 </template>
 <script>
-export default {};
+import Footer from "@/components/footer";
+export default {
+  components: {
+    Footer,
+  },
+};
 </script>
 <style scoped>
 .main {
   height: 50em;
-  background: center/100% no-repeat url("../assets/main_page_background.jpg");
+  background: center/cover no-repeat url("../assets/main_page_background.jpg");
   background-size: cover;
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+@media screen and (max-width: 48rem) {
+  .main {
+    height: 40em;
+    background: 58% / cover no-repeat url("../assets/main_page_background.jpg");
+  }
 }
 </style>
