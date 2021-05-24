83a7b4f56854dac0edbb673874e00d83415702c8
jangjunhyeok <95jjangjun@gmail.com>
2021-05-24 17:10:37 +0900

    Add fontawesome icon

--- src/utils/fontawesome.js
+++ src/utils/fontawesome.js
@@ -1,8 +1,8 @@
 import Vue from "vue";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faBars, faWindowClose } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faWindowClose, faUser, faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faBars, faWindowClose);
+library.add(faBars, faWindowClose, faUser, faChevronDown);
 
 Vue.component("font-awesome-icon", FontAwesomeIcon);
