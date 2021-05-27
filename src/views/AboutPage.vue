ee79dcd72712e8ab2a58f5751b973b9e35aeef5e
jangjunhyeok <95jjangjun@gmail.com>
2021-05-27 09:08:04 +0900

    About page 수정

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -1,10 +1,6 @@
 <template>
   <section class="main">
-    <p>
-      곧 날이 더워질텐데 시원한 분위기를 주고싶어서 파랑 파랑한 배경을 사용해보았습니다.<br />
-      그동안 간단한 소규모 프로젝트 (ex. to-do, portfolio)의 경험 밖에 없어서 <br />
-      HappyHouse는 제가 처음으로 진행한 조금 규모가 있는 프로젝트입니다. <br />
-    </p>
+    <p>20, 30대를 위해 조금이나마 도움이 되었으면 좋겠습니다. <br /></p>
   </section>
 </template>
 <script>
@@ -18,5 +14,13 @@ export default {};
   display: flex;
   justify-content: center;
   align-items: center;
+  color: whitesmoke;
+}
+
+@media screen and (max-width: 24rem) {
+  p {
+    width: 80%;
+    text-align: center;
+  }
 }
 </style>
